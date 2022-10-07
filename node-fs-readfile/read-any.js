6fa7072e5a230ce8a47cdc2749c727b94e5babb5
Morris Wang <morris1214@gmail.com>
2022-10-07 02:15:04 +0000

    Ameneded node-fs-readfile level 2

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -2,30 +2,9 @@ const fs = require('fs');
 const argv = process.argv;
 const currentFile = argv[2];
 
-fs.readFile('./hopper.txt', 'utf8', (err, data) => {
+fs.readFile(currentFile, 'utf8', (err, data) => {
   if (err) throw err;
-  if (currentFile === 'hopper.txt') {
-    console.log(data);
-  }
-});
-
-fs.readFile('./cunningham.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  if (currentFile === 'cunningham.txt') {
-    console.log(data);
-  }
-});
-
-fs.readFile('./hipster-ipsum.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  if (currentFile === 'hipster-ipsum.txt') {
-    console.log(data);
-  }
-});
-
-fs.readFile('./dijkstra.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  if (currentFile === 'dijkstra.txt') {
+  if (currentFile) {
     console.log(data);
   }
 });
