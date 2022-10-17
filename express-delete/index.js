6132b0b6d896901159061d887fcdcbeb8c37bea9
Morris Wang <morris1214@gmail.com>
2022-10-17 02:21:44 +0000

    Completed express-delete exercise

---
+++ express-delete/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
+
+app.get('/api/grades', (req, res) => {
+  const array = Object.values(grades);
+  res.json(array);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[Number(req.params.id)];
+  res.sendStatus(204);
+});
