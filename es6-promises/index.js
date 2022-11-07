92d1e4dc7da1f3408012b69bb18cca1b4d3aebb5
Morris Wang <morris1214@gmail.com>
2022-11-07 00:11:23 +0000

    Completed es6-promises exercise

---
+++ es6-promises/index.js
@@ -0,0 +1,15 @@
+const takeAChance = require('./take-a-chance');
+
+const myPromise = takeAChance('Morris');
+
+myPromise.then(name => {
+  console.log(`${name}`);
+  throw new Error();
+})
+  .catch(e => {
+    console.log(e.message);
+  })
+  .then(
+    () => console.log('Horray! You\'re so lucky, ' + `${name}`),
+    () => console.log('It\'s just bad luck, ' + `${name}`)
+  );
